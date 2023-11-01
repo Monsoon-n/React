a7cdb208c1764b5d44fa5d3c3e1c5211dc6c73dc
🍔 <1163990297@qq.com>
2023-11-01 18:28:40 +0800

    计算器2

--- src/page/calculator/calculator.js
+++ src/page/calculator/calculator.js
@@ -1,65 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Calculator.css";
-function Calculator({ value1, value2, operator, result }) {
-  const defaultoption = "+";
-  operator = defaultoption;
-  function Input() {
+function Calculator() {
+  function Calculator1({ which }) {
+    let value1, value2, operator;
+    const [result, setResult] = useState();
+    const defaultoption = "+";
+    operator = defaultoption;
+    const equal = () => {
+      // if (operator === "+") result = Number(value1) + Number(value2);
+      if (operator === "+") setResult(() => Number(value1) + Number(value2));
+      if (operator === "-") result = Number(value1) - Number(value2);
+      if (operator === "*") result = Number(value1) * Number(value2);
+      if (operator === "/" && Number(value2) !== 0)
+        result = Number(value1) / Number(value2);
+      if (operator === "/" && Number(value2) === 0) alert("sb");
+      console.log(which + result);
+    };
     return (
-      <input
-        type="text"
-        className="Num1"
-        onChange={(e) => {
-          value1 = e.target.value;
-          console.log(value1);
-        }}
-      />
+      <div>
+        <input
+          type="text"
+          className="Num1"
+          onChange={(e) => {
+            value1 = e.target.value;
+          }}
+        />
+        <select
+          defaultValue={defaultoption}
+          className="Operator"
+          onChange={(e) => (operator = e.target.value)}
+        >
+          <option value="+">+</option>
+          <option value="-">-</option>
+          <option value="*">*</option>
+          <option value="/">/</option>
+        </select>
+        <input
+          type="text"
+          className="Num2"
+          onChange={(e) => {
+            value2 = e.target.value;
+          }}
+        />
+        <button className="Equal" onClick={equal}>
+          =
+        </button>
+        <input type="text" className="Result" value={result} />
+      </div>
     );
   }
-  function Input2() {
-    return (
-      <input
-        type="text"
-        className="Num2"
-        onChange={(e) => {
-          value2 = e.target.value;
-          console.log(value2);
-        }}
-      />
-    );
-  }
-  // function Input3() {
-  //   return <input type="text" className="Result" value={result} />;
-  // }
-  const equal = (e) => {
-    e.preventDefault();
-    if (operator === "+") result = Number(value1) + Number(value2);
-    if (operator === "-") result = Number(value1) - Number(value2);
-    if (operator === "*") result = Number(value1) * Number(value2);
-    if (operator === "/" && Number(value2) !== 0)
-      result = Number(value1) / Number(value2);
-    if (operator === "/" && Number(value2) === 0) alert("sb");
-    console.log(result);
-  };
   return (
     <div>
-      <Input />
-      <select
-        defaultValue={defaultoption}
-        className="Operator"
-        onChange={(e) => (operator = e.target.value)}
-      >
-        <option value="+">+</option>
-        <option value="-">-</option>
-        <option value="*">*</option>
-        <option value="/">/</option>
-      </select>
-      <Input2 />
-      <button className="Equal" onClick={equal}>
-        =
-      </button>
-      <input type="text" className="Result" value={result} />
+      <Calculator1 which={"一号"} />
+      <Calculator1 which={"二号"} />
     </div>
   );
 }
-
 export default Calculator;
