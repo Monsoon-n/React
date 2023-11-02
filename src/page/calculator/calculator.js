82da0151f49606a104c2828c07cee00431d296f8
🍔 <1163990297@qq.com>
2023-11-03 01:09:32 +0800

    the final

--- src/page/calculator/calculator.js
+++ src/page/calculator/calculator.js
@@ -1,71 +1,80 @@
 import React, { useState } from "react";
 import "./Calculator.css";
+function Final({ which, result }) {
+  return (
+    <div>
+      {which}: {result}
+    </div>
+  );
+}
+function Calculator1({ which, result, setResult }) {
+  const [operator, setOperator] = useState("+");
+  const [value1, setValue1] = useState("");
+  const [value2, setValue2] = useState("");
+  // console.log(which);
 
-function Calculator() {
-  function Calculator1({ which }) {
-    const [result, setResult] = useState("");
-    const [operator, setOperator] = useState("+");
-    const [value1, setValue1] = useState("");
-    const [value2, setValue2] = useState("");
-    const equal = () => {
-      if (operator === "+") {
-        setResult(() => Number(value1) + Number(value2));
-      }
-      if (operator === "-") {
-        setResult(() => Number(value1) - Number(value2));
-      }
-      if (operator === "*") {
-        setResult(() => Number(value1) * Number(value2));
-      }
-      if (operator === "/" && Number(value2) !== 0) {
-        setResult(() => Number(value1) / Number(value2));
-      }
-      if (operator === "/" && Number(value2) === 0) {
-        alert("sb");
-      }
-      console.log(which + result);
-    };
-    function Final() {
-      return <div>1号: {result}</div>;
+  const equal = () => {
+    console.log(which);
+    if (operator === "+") {
+      setResult(() => Number(value1) + Number(value2));
     }
-    return (
-      <div>
-        <input
-          type="text"
-          className="Num1"
-          onChange={(e) => {
-            setValue1(e.target.value);
-          }}
-        />
-        <select
-          className="Operator"
-          onChange={(e) => setOperator(e.target.value)}
-        >
-          <option value="+">+</option>
-          <option value="-">-</option>
-          <option value="*">*</option>
-          <option value="/">/</option>
-        </select>
-        <input
-          type="text"
-          className="Num2"
-          onChange={(e) => {
-            setValue2(e.target.value);
-          }}
-        />
-        <button className="Equal" onClick={equal}>
-          =
-        </button>
-        <input type="text" className="Result" value={result} />
-        <Final />
-      </div>
-    );
-  }
+    if (operator === "-") {
+      setResult(() => Number(value1) - Number(value2));
+    }
+    if (operator === "*") {
+      setResult(() => Number(value1) * Number(value2));
+    }
+    if (operator === "/" && Number(value2) !== 0) {
+      setResult(() => Number(value1) / Number(value2));
+    }
+    if (operator === "/" && Number(value2) === 0) {
+      alert("sb");
+    }
+  };
 
   return (
     <div>
-      <Calculator1 which={"一号"} />
-      <Calculator1 which={"二号"} />
+      <input
+        type="text"
+        className="Num1"
+        onChange={(e) => {
+          setValue1(e.target.value);
+        }}
+      />
+      <select
+        className="Operator"
+        onChange={(e) => setOperator(e.target.value)}
+      >
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+      </select>
+      <input
+        type="text"
+        className="Num2"
+        onChange={(e) => {
+          setValue2(e.target.value);
+        }}
+      />
+      <button className="Equal" onClick={equal}>
+        =
+      </button>
+      <input type="text" className="Result" value={result} />
+    </div>
+  );
+}
+function Calculator() {
+  const [result1, setResult1] = useState("");
+  const [result2, setResult2] = useState("");
+  const which1 = "一号";
+  const which2 = "二号";
+  return (
+    <div>
+      <Calculator1 which={which1} result={result1} setResult={setResult1} />
+      <Calculator1 which={which2} result={result2} setResult={setResult2} />
+      <Final which={which1} result={result1} />
+      <Final which={which2} result={result2} />
     </div>
   );
 }
